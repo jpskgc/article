e8be59796f8a773cb8944c4d36247ebf4a7c9563
jpskgc <jpskgc.v3@gmail.com>
2019-09-10 23:48:45 +0900

    refactor handler

--- api/main.go
+++ api/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"article/api/dao"
 	"article/api/db"
+	"article/api/handler"
 	"article/api/s3"
 	"article/api/service"
+	"log"
 	"os"
-	"time"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/joho/godotenv"
@@ -20,6 +19,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
 	db := db.NewDatabase(os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
@@ -27,42 +27,7 @@ func main() {
 	dao := dao.NewDao(db.DATABASE, s3)
 	service := service.NewService(dao)
 	cntlr := controller.NewController(service)
+	router := handler.NewHandler(cntlr)
 
-	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return true
-		},
-		MaxAge: 15 * time.Second,
-	}))
-
-	api := router.Group("/api")
-	{
-		api.GET("/articles", func(c *gin.Context) {
-			cntlr.GetArticleController(c)
-		})
-		api.GET("/article/:id", func(c *gin.Context) {
-			cntlr.GetSingleArticleController(c)
-		})
-		api.GET("/delete/:id", func(c *gin.Context) {
-			cntlr.DeleteArticleController(c)
-		})
-		api.POST("/post", func(c *gin.Context) {
-			cntlr.PostController(c)
-		})
-		api.POST("/post/image", func(c *gin.Context) {
-			cntlr.PostImageController(c)
-		})
-		api.POST("/post/image/db", func(c *gin.Context) {
-			cntlr.PostImageToDBController(c)
-		})
-	}
-
-	router.Run(":2345")
+	router.Router.Run(":2345")
 }
